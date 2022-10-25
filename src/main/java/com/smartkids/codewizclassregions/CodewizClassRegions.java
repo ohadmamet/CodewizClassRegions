7e81b6d788e1354c7779eb6072889d912f01f76d
Ohad Mamet <ohadmamet@gmail.com>
2022-10-25 18:12:33 -0400

    add boilerplate

---
+++ src/main/java/com/smartkids/codewizclassregions/CodewizClassRegions.java
@@ -0,0 +1,17 @@
+package com.smartkids.codewizclassregions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CodewizClassRegions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
