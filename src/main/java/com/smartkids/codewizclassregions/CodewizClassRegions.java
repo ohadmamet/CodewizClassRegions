950f8f18820163795eb47244670d02e78ccc590e
clayton14 <claytoneasley77@gmail.com>
2022-11-06 00:46:01 -0400

    registering events

--- src/main/java/com/smartkids/codewizclassregions/CodewizClassRegions.java
+++ src/main/java/com/smartkids/codewizclassregions/CodewizClassRegions.java
@@ -2,12 +2,15 @@ package com.smartkids.codewizclassregions;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.smartkids.codewizclassregions.events.Regions;
+
 public final class CodewizClassRegions extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         getLogger().info("------> HELLO WORLD");
+        getServer().getPluginManager().registerEvents(new Regions(), this);
     }
 
     @Override
